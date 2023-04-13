39d9f21aa75364f396242f2f19e17bd35cf6983a
Александр Ячменев <yachmenyov.alex@yandex.ru>
2023-04-14 01:39:19 +0700

    Moved api_yambd\api\admin.py to api_yambd\reviews\admin.py.Registration of the admin model for the "reviews" app.

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Review, Title, Category, Genre
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    """Админ-модель отзывов."""
+    list_display = (
+        'pk',
+        'title',
+        'text'
+        'author',
+        'score',
+        'pub-date'
+    )
+    list_fields = ('title', 'text', 'author')
+    list_editable = ('score',)
+    list_filter = ('pub_date',)
+
+
+class TitleAdmin(admin.ModelAdmin):
+    """Админ-модель произведений."""
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'description',
+        'category',
+    )
+    list_editable = ('description',)
+    search_fields = ('name',)
+
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Админ-модель категорий."""
+    pass
+
+
+class GenreAdmin(admin.ModelAdmin):
+    """Админ-модель жанров."""
+    pass
+
+
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
