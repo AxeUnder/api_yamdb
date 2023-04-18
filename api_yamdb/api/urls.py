ba7ce5a06344445780e10ef908faaa2802aa3444
Oleg Li <oleg.giri@gmail.com>
2023-04-18 12:45:50 +0700

    add viewset fot genre, category

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,26 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import TitleViewSet, CategoryViewSet, GenreViewSet, ReviewViewSet, CommentViewSet
+from .views import (
+    CategoryViewSet,
+    CommentViewSet,
+    GenreViewSet,
+    ReviewViewSet,
+    TitleViewSet,
+)
 
 router_v1 = routers.DefaultRouter()
 router_v1.register('titles', TitleViewSet, basename='titles')
-router_v1.register(r'titles/(?P<title_id>\d+)/reviews/', ReviewViewSet, basename='reviews')
-router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/', CommentViewSet, basename='comments')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/',
+    ReviewViewSet,
+    basename='reviews',
+)
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments/',
+    CommentViewSet,
+    basename='comments',
+)
 router_v1.register('categories', CategoryViewSet, basename='categories')
 router_v1.register('genres', GenreViewSet, basename='genres')
 
